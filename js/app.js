4bd87796d223edaac9c27cbfec3ad22796fae98a
romerojoseing <romerojoseing@gmail.com>
2020-10-21 22:36:36 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
+/* Template Name: Landrick - Saas & Software Landing Page Template
+   Author: Shreethemes
+   E-mail: shreethemes@gmail.com
+   Created: August 2019
+   Version: 2.5
+   Updated: July 2020
+   File Description: Main JS file of the template
+*/
+
+/****************************/
+/*         INDEX            */
+/*===========================
+ *     01.  Loader          *
+ *     02.  Menu            *
+ *     03.  Sticky Menu     *
+ *     03.  Back to top     *
+ ===========================*/
+
 ! function($) {
     "use strict"; 
-
+    // Loader 
+    $(window).on('load', function() {
+        $('#status').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }); 
     
     // Menu
     $('.navbar-toggle').on('click', function (event) {
@@ -78,5 +103,6 @@
     $(function () {
         $('[data-toggle="popover"]').popover()
     });
-
+    //Feather icon
+    feather.replace()
 }(jQuery)
